0fda162dd9c44de3334ab9fcf146e72f9ff53a4d
Yves-Tabor <walvesoutis@gmail.com>
2026-05-27 23:42:51 +0200

    frontend structure..

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const auth = require("./../middleware/auth")
 
-const { createPost, updatePost, likePost, deletePost } = require("./../controllers/postController")
+const { createPost, updatePost, likePost, deletePost, getPosts } = require("./../controllers/postController")
 
 router.post("/", auth, createPost)
 router.put("/:id", auth, updatePost)
 router.patch("/:id/like", auth, likePost)
 router.delete("/:id", auth, deletePost)
+router.get("/", getPosts);
 
 module.exports = router;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <div className="bg-black text-white p-10">
+      <h1 className="text-5xl font-bold">
+        CampusHub
+      </h1>
+    </div>
+  )
+}
+
+
+export default App
